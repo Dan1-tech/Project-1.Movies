9d981d8b53cd6e22e1e548e4ba3c6a796aafd651
Dan1-tech <dan250897@iCloud.com>
2025-02-22 16:26:55 +0200

    Add functions Genre

--- main.js
+++ main.js
@@ -2,40 +2,80 @@
 `use strict`;
 
 
-const numberOfFilms = +prompt(`How many movies have you watched?`);
+let numberOfFilms;
     
+
+function start(){
+    numberOfFilms = +prompt(`How many movies have you watched?`);
+
+     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt(`How many movies have you watched?`);
+    }
+}
+start();
+
+
 const personalMovieDB = {
     count : numberOfFilms,
     movies: {},
     actors : {},
     genres : [],
-    privat : false
+    privat : true
 };
 
 
-for (let i = 0;i < 2; i++ ) {
-    const  a = prompt(`What is the last movie you watched?`);
-           b = prompt(`What is your rating for the movie?`);
-      
-    if(a != null && b != null && a != `` && b != `` && a.length < 50){
-        personalMovieDB.movies[a] = b;
-    }else {
-        console.log(`error`);
-        i--;
+
+
+function rememberMyFilms(){
+    for (let i = 0;i < 2; i++ ) {
+        const  a = prompt(`What is the last movie you watched?`);
+               b = prompt(`What is your rating for the movie?`);
+          
+        if(a != null && b != null && a != `` && b != `` && a.length < 50){
+            personalMovieDB.movies[a] = b;
+        }else {
+            console.log(`error`);
+            i--;
+        }
+        
     }
-    
 }
 
-if (personalMovieDB.count < 10){
-    console.log(`You watch little movies`);
-}else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
-    console.log(`You are kino critic`);
-}else if (personalMovieDB.count >= 30){
-    console.log(`You are kino maniac`);
-}else{
-    console.log(`Error`);
+rememberMyFilms();
+
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10){
+        console.log(`You watch little movies`);
+    }else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+        console.log(`You are kino critic`);
+    }else if (personalMovieDB.count >= 30){
+        console.log(`You are kino maniac`);
+    }else{
+        console.log(`Error`);
+    }
+        
 }
-    
+
+detectPersonalLevel();
+
+
+function showMyDB (){
+    if (personalMovieDB.privat == false){
+        console.log(`General object`);
+    }else {
+        console.log(`Not General object`);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres(){
+    for (let i = 1; i < 4; i++){
+        personalMovieDB.genres[i - 1] =  prompt(`Your favourite genre under number ${i}`);    
+    }
+}
+
+writeYourGenres();
 
 
 console.log(personalMovieDB);
